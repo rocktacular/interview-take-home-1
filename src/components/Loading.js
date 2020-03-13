5eb139997efa1f81ef76247d6a05a932cfe910a8
Colin Folta <colin.folta@gmail.com>
2020-03-13 14:45:13 -0500

    Revert "add mobx-react."
    This reverts commit 4ee5c769f985c95d2bcc1fb6f3964a5ac0675d05.

--- src/components/Loading.js
+++
@@ -1,6 +0,0 @@
-import React from "react";
-import "./Loading.css";
-
-const Loading = () => <div className="loading">LOADING...</div>;
-
-export default Loading;
--- src/screens/Details.js
+++ src/screens/Details.js
@@ -1,22 +1,19 @@
-import React, { useEffect, useState, useContext } from "react";
+import React, { useEffect, useState } from "react";
 import { useParams, useLocation } from "react-router-dom";
-import { useObserver } from "mobx-react-lite";
-
-import { StoreContext } from "../stores/AppStore";
-
 import "./Details.css";
 import { get } from "lodash";
+import TMDB from "../services/TMDB";
 import Score from "../components/Score";
 import Favorite from "../components/Favorite";
-import Loading from "../components/Loading";
 
 function Details() {
   // FETCH DATA
   const { id } = useParams();
-  const store = useContext(StoreContext);
   useEffect(() => {
-    store.fetchMovieDetails(id);
-  }, [id]);
+    TMDB.details(id).then(res => {
+      setDetails(res);
+    });
+  }, []);
 
   // ROUTE PROPS
   const location = useLocation();
@@ -26,34 +23,30 @@ function Details() {
 
   // STATE
   const [isFavorite, setIsFavorite] = useState(false);
+  const [details, setDetails] = useState("");
 
   // HANDLERS
   const clickFavorite = () => {
     setIsFavorite(!isFavorite);
   };
-  return useObserver(() => (
+  return (
     <div className="details-page">
-      {store.isLoading ? (
-        <Loading />
-      ) : (
-        <>
-          <div className="details__top">
-            <img className="details__image" src={imageUrl} alt="Movie Poster" />
-            <div className="details__info">
-              <div className="details__info-top">
-                <Score score={score} />
-                <Favorite isFavorite={isFavorite} onClick={clickFavorite} />
-              </div>
-              <div className="details__info-bottom">
-                <div>{releaseDate}</div>
-              </div>
-            </div>
+      <div className="details__top">
+        <img className="details__image" src={imageUrl} alt="Movie Poster" />
+
+        <div className="details__info">
+          <div className="details__info-top">
+            <Score score={score} />
+            <Favorite isFavorite={isFavorite} onClick={clickFavorite} />
           </div>
-          <div className="details__bottom">{store.movieDetails.overview}</div>
-        </>
-      )}
+          <div className="details__info-bottom">
+            <div>{releaseDate}</div>
+          </div>
+        </div>
+      </div>
+      <div className="details__bottom">{details.overview}</div>
     </div>
-  ));
+  );
 }
 
 export default Details;
